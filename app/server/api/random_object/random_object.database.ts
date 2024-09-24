f597be82d64f425fe21c7c5bf87cad4f10813731
Gustavo Gardusi <gustavo.gardusi@gmail.com>
2024-09-23 21:06:32 -0300

    Refactor structure (#15)

---
+++ app/server/api/random_object/random_object.database.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaClient, RandomObject } from '@prisma/client'
+import { AbstractDatabase } from '../abstract/abstract.database'
+
+export const RandomObjectDatabaseToken = 'RandomObjectDatabase'
+
+@Injectable()
+export class RandomObjectDatabase extends AbstractDatabase<RandomObject> {
+  constructor() {
+    super(new PrismaClient().randomObject, 'id')
+  }
+}
